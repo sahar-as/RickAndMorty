bc60c4859d42d6492a83bf49b70b0fc48269cc69
sahar-as <sahar.asadian90@gmail.com>
2023-06-13 17:02:48 +0330

    changing name of items in xml

--- app/src/main/java/ir/saharapps/rickandmorty/data/remote/RickAndMortyApi.kt
+++ app/src/main/java/ir/saharapps/rickandmorty/data/remote/RickAndMortyApi.kt
@@ -1,9 +1,10 @@
 package ir.saharapps.rickandmorty.data.remote
 
+import ir.saharapps.rickandmorty.data.dto.RickAndMortyDto
 import retrofit2.http.GET
 
 interface RickAndMortyApi {
 
     @GET("character")
-    suspend fun getCharacter()
+    suspend fun getCharacter(): RickAndMortyDto
 }
