7d7b5334b6834152f5cbd636aba03113345c60b8
sahar-as <sahar.asadian90@gmail.com>
2023-06-19 13:48:11 +0330

    add toolbar & make change on home fragment xml

--- app/src/main/java/ir/saharapps/rickandmorty/MainActivity.kt
+++ app/src/main/java/ir/saharapps/rickandmorty/MainActivity.kt
@@ -2,10 +2,13 @@ package ir.saharapps.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import ir.saharapps.rickandmorty.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -13,9 +16,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment= supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
     }
 }
--- app/src/main/java/ir/saharapps/rickandmorty/ui/screen/characters/CharactersFragment.kt
+++ app/src/main/java/ir/saharapps/rickandmorty/ui/screen/characters/CharactersFragment.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import ir.saharapps.rickandmorty.R
 import ir.saharapps.rickandmorty.databinding.FragmentCharactersBinding
@@ -33,8 +35,10 @@ class CharactersFragment: Fragment(R.layout.fragment_characters) {
         charactersViewModel.getCharacters()
 
         binding.rvCharacters.apply {
-            layoutManager = GridLayoutManager(this.context, 2)
+            layoutManager = LinearLayoutManager(this.context)
             adapter = charactersAdapter
+            val dividerItemDecoration = DividerItemDecoration(this.context, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
         }
 
         lifecycleScope.launch{
